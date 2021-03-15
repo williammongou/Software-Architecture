1a719ea6131493396bc546ab9e6cb8abd15b24e6
William Mongou <wmongou@uco.edu>
2021-03-15 16:34:08 -0500

    initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Main{
+
+	public static void main(String [] args){
+
+		JFrame window = new JFrame();
+
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setPreferredSize(new Dimension(800,600));
+
+		
+	}
+}
---
+++ model/LineStorage.java
@@ -0,0 +1,10 @@
+package model;
+
+public interface LineStorage {
+
+	void setChar(char c);
+	char getChar();
+	int wordCount();
+
+	
+}
